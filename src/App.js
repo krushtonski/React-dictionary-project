c4a3e890649f29401c53d1dd169853aaa2f5003d
krushtonski <36168072+krushtonski@users.noreply.github.com>
2021-04-16 10:05:39 +0100

    Added Search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header"></header>
+      </div>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Kate Rushton</small>
+      </footer>
     </div>
   );
 }
