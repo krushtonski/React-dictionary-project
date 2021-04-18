b969628c7c1662e22233ed82df817acf01a6e6d3
krushtonski <36168072+krushtonski@users.noreply.github.com>
2021-04-18 19:30:52 +0100

    Added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -8,9 +9,12 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition</strong>
               {definition.definition}
               <br />
+              <strong>Example:</strong>
               <em>{definition.example}</em>
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
