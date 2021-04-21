16ece8357fa094858532c1445c4ef87ea95b45a5
krushtonski <36168072+krushtonski@users.noreply.github.com>
2021-04-21 10:25:58 +0100

    Error correction

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,7 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-12 col-md-4" key={index}>
-                <a href={photo.src.original} target="_blank">
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
                   <img
                     src={photo.src.landscape}
                     alt="images"
